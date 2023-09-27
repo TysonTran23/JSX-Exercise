78aafc777b8108ba7f57744184f95b8feeb22d1a
Tyson <trantyson@icloud.com>
2023-09-27 14:15:21 -0700

    finish part 2

--- part-2/App.js
+++ part-2/App.js
@@ -1,11 +1,25 @@
 const App = () => {
   return (
-    <Tweet
-      username="ttran"
-      name="tykan"
-      date="september"
-      message="I want to be rich"
-    />
+    <div>
+      <Tweet
+        username="ttran"
+        name="tykan"
+        date="september"
+        message="I want to be rich"
+      />
+      <Tweet
+        username="kitty"
+        name="mari"
+        date="september"
+        message="I want to be rich"
+      />
+      <Tweet
+        username="deek"
+        name="alex"
+        date="september"
+        message="I want to be jacked"
+      />
+    </div>
   );
 };
 
