891f8e111b8c6342af243b4f87e000ac81421521
Tyson <trantyson@icloud.com>
2023-09-27 14:04:05 -0700

    finish part1

---
+++ part-1/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <FirstComponent />
+      <Name name="TTRAN"/>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ part-1/NamedComponent.js
@@ -0,0 +1,3 @@
+const Name = (props) => {
+  return <h1>My name is {props.name}</h1>;
+};
